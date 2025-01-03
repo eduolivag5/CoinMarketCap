a491030eeaf77ed50e3e7b3e2b3478a3d2e5f3dc
eduolivag5 <eduolivag5@gmail.com>
2025-01-03 18:08:51 +0100

    changed logo on homePage

--- react/src/pages/Home.tsx
+++ react/src/pages/Home.tsx
@@ -5,7 +5,9 @@ export default function Home() {
     
 
     return (
-        <div className="mx-auto max-w-6xl space-y-6">            
+        <div className="mx-auto max-w-6xl space-y-6">   
+
+            <p className="py-10 drop-shadow-[0px_0px_55px_rgba(98,56,214,0.9)] text-4xl md:text-6xl font-bold text-center">CoinMarketCap</p>         
 
             {/* Mostrar Highlights solo si el interruptor está activado */}
             <Highlights />
